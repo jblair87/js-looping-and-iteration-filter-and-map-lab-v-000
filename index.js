8f8543ca330e05b912636e99ec3feba11bdfc1b4
Jenna Mandel <jenna.b.mandel@gmail.com>
2018-10-05 18:33:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function driverNamesWithRevenueOver(drivers, revenue){
 function exactMatch(driver, matchers){
    return drivers.filter(function(driver){
      let matches == false;
-     for(const key in matches)
+     for(const key in matchers)
      
      
    }
