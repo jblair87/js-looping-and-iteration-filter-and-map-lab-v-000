52cf4f3567530aaf20d2778462f3a5b535ff07d7
Jenna Mandel <jenna.b.mandel@gmail.com>
2018-10-05 17:52:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ function driverNamesWithRevenueOver(driver){
   
 }
 function exactMatch(driver){
+  return list.filter(function (driverName) {
+return driverName.toLowerCase() === name.toLowerCase();
+});
+}
+
   
 }
 function exactMatchToList(driver){
